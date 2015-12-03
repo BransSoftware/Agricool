6f5cc70888b375e3952c08288d26fa416e9fd6f5
Nicolas Beaudrot <galip39@gmail.com>
2015-12-03 16:57:08 +0100

    Operation: fillFromDb

--- src/dao/operationdao.cpp
+++ src/dao/operationdao.cpp
@@ -8,6 +8,19 @@ OperationDao::OperationDao(DbService * parent, QSqlDatabase db)
 
 Operation* OperationDao::fillFromDb(QSqlRecord record)
 {
-    //return new Operation(record.value(1));
+    /*CultureCycle* cultureCycle = dbService->getDao<CultureCycleDao>()->get(record.value(1).toInt());
+    
+    QDateTime date;
+    date.setTime_t(record.value(3).toUInt());
+    
+    return new Operation(record.value(0).toInt(), //id
+        cultureCycle, // culture cycle
+        record.value(2).toString(), // name
+        date, // date
+        record.value(4).toInt(), // duration
+        record.value(5).toInt(), // employee work hour
+        static_cast<OperationType>(record.value(6).toInt()), // quantity unit
+        record.value(7).toString() // comment
+    );*/
     return NULL;
 }
