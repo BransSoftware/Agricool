c8b667f86bb9ec20950b605d36ec26bd0fc4f1af
Nicolas Beaudrot <galip39@gmail.com>
2015-12-03 16:45:35 +0100

    Harvest: fillFromDb

--- src/dao/harvestdao.cpp
+++ src/dao/harvestdao.cpp
@@ -8,9 +8,15 @@ HarvestDao::HarvestDao(DbService * parent, QSqlDatabase db)
 
 Harvest* HarvestDao::fillFromDb(QSqlRecord record)
 {
-    CultureCycle* cultureCycle = dbService->getDao<CultureCycleDao>()->get(record.value(1).toInt());
+    /*CultureCycle* cultureCycle = dbService->getDao<CultureCycleDao>()->get(record.value(1).toInt());
+    
     return new Harvest(record.value(0).toInt(), // id
-        
-    );
+        cultureCycle, // culture cycle
+        record.value(2).toString(), // plant name
+        record.value(3).toInt(), // quantity
+        record.value(4).toInt(), // income per quantity unit
+        static_cast<QuantityUnit>(record.value(5).toInt()), // quantity unit
+        record.value(6).toString() // comment
+    );*/
     return NULL;
 }
