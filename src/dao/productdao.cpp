990dc6b4272f2053f7e9bd74d5f496fd6675a649
Nicolas Beaudrot <galip39@gmail.com>
2015-12-03 17:02:23 +0100

    Product: fillFromDb

--- src/dao/productdao.cpp
+++ src/dao/productdao.cpp
@@ -8,6 +8,12 @@ ProductDao::ProductDao(DbService * parent, QSqlDatabase db)
 
 Product* ProductDao::fillFromDb(QSqlRecord record)
 {
-    //return new Product(record.value(1));
+    /*return new Product(record.value(0).toInt(), // id
+        record.value(1).toString(), // name
+        static_cast<QuantityUnit>(record.value(2).toInt()), // unit
+        record.value(3).toInt(), // price per quantity
+        static_cast<ProductType>(record.value(4).toInt()), // type
+        this
+    );*/
     return NULL;
 }
