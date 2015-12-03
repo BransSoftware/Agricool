485ed2d94e51ac258cf1b35d973c566104c3b409
Nicolas Beaudrot <galip39@gmail.com>
2015-12-03 17:05:33 +0100

    ProductUsage: fillFromDb

--- src/dao/productusagedao.cpp
+++ src/dao/productusagedao.cpp
@@ -8,6 +8,13 @@ ProductUsageDao::ProductUsageDao(DbService * parent, QSqlDatabase db)
 
 ProductUsage* ProductUsageDao::fillFromDb(QSqlRecord record)
 {
-    //return new ProductUsage(record.value(1));
+    /*Operation* operation = dbService->getDao<OperationDao>()->get(record.value(1).toInt());
+    Product* product = dbService->getDao<ProductDao>()->get(record.value(2).toInt());
+    
+    return new ProductUsage(record.value(0).toInt(), // id
+        operation, // operation
+        product, // product
+        record.value(3).toInt() // quantity used
+    );*/
     return NULL;
 }
