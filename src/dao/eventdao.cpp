4056a1d6102fec5815b7063beb98870c96d73a24
Nicolas Beaudrot <galip39@gmail.com>
2015-12-03 16:40:59 +0100

    Event: fillFromDb

--- src/dao/eventdao.cpp
+++ src/dao/eventdao.cpp
@@ -8,6 +8,16 @@ EventDao::EventDao(DbService * parent, QSqlDatabase db)
 
 Event* EventDao::fillFromDb(QSqlRecord record)
 {
-    //return new Event(record.value(1));
+    /*CultureCycle* cultureCycle = dbService->getDao<CultureCycleDao>()->get(record.value(1).toInt());
+    
+    return new Event(record.value(0).toInt(), // id
+        cultureCycle, // culture cycle
+        record.value(2).toInt(), // financial income
+        record.value(3).toInt(), // financial cost
+        record.value(4).toInt(), // area gain
+        record.value(5).toInt(), // area lost
+        record.value(6).toString(), // description
+    );*/
+    
     return NULL;
 }
